9150cb648096b3b30df764ce473df7b1c3a298a1
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-09-16 00:06:25 +0700

    navbar styling done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './navbar.css'
+import { BsHeartFill } from 'react-icons/bs'
+import { FaCartShopping } from 'react-icons/fa6'
 
 const Navbar = () => {
   return (
@@ -8,8 +10,8 @@ const Navbar = () => {
         <div className='navbar-support'>Support +62 1123 456</div>
         <div className='navbar-title'>NiceCommerce</div>
         <div className='navbar-icons'>
-          <div>1</div>
-          <div>2</div>
+          <div className='navbar-icons-search'><BsHeartFill/></div>
+          <div className='navbar-icons-cart'><FaCartShopping/></div>
         </div>
         <button className='navbar-login'>Login</button>
       </div>    
